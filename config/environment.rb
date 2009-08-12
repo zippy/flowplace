19e2595864f876da2c173eaeeac9bddd563ae27a
Eric Harris-Braun <eric@harris-braun.com>
2009-08-12 10:14:49 -0400

    updated readme to rdoc format

--- config/environment.rb
+++ config/environment.rb
@@ -31,6 +31,7 @@ Rails::Initializer.run do |config|
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem 'gravtastic'
+  config.gem 'nokogiri'
   # Only load the plugins named here, in the order given. By default, all plugins 
   # in vendor/plugins are loaded in alphabetical order.
   # :all can be used as a placeholder for all plugins not explicitly named
