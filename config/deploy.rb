6829f981df33eda483b899dbe64baa5ddb49f75b
Eric Harris-Braun <eric@harris-braun.com>
2009-08-12 10:33:23 -0400

    updates to deploy script

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,10 +1,39 @@
 require ENV['DEPREC_PATH'] ? ENV['DEPREC_PATH'] : 'deprec'
-  
-set :application, "flowplace"
-set :domain, "dev.flowplace.org"
+
+subdomain = ENV['CAP_DOMAIN']
+
+
+if subdomain && subdomain != ''
+  set :domain, subdomain+'.flowplace.org'
+else
+  set :domain, "dev.flowplace.org"
+end
+
+case domain
+when 'demo.flowplace.org'
+  set :user, "ehb"
+  set :application, 'flowplace_demo'
+  set :ssh_port,22
+when 'seed.flowplace.org'
+  set :user, "ehb"
+  set :application, 'flowplace_seed'
+  set :ssh_port,22
+when 'dev.flowplace.org'
+  set :user, "eric"
+  set :application, 'flowplace'
+  set :ssh_port,22
+end
+
+puts "*** Domain: \033[1;41m #{domain} \033[0m"
+
+ssh_options[:port] = ssh_port
+
 set :repository,  "git://github.com/zippy/flowplace.git"
 
 set :gems_for_project, %w(gravtastic) # list of gems to be installed
+
+set :deploy_to, "/opt/apps/#{application}"
+set :ruby_vm_type,      :ree        # :ree, :mri
 
 # If you aren't using Subversion to manage your source code, specify
 # your SCM below:
@@ -14,7 +43,6 @@ set :git_enable_submodules, true
 set :database_yml_in_scm, false
 set :passenger_use_mod_rewrite_for_disable, false
    
-set :ruby_vm_type,      :ree        # :ree, :mri
 set :web_server_type,   :apache     # :apache, :nginx
 set :app_server_type,   :passenger  # :passenger, :mongrel
 set :db_server_type,    :mysql      # :mysql, :postgresql, :sqlite
@@ -27,13 +55,32 @@ role :app, domain
 role :web, domain
 role :db,  domain, :primary => true
 
-# If you aren't deploying to /opt/apps/#{application} on the target
-# servers (which is the deprec default), you can specify the actual location
-# via the :deploy_to variable:
-set :deploy_to, "/opt/apps/#{application}"
+after 'deploy:symlink', :roles => :app do
+  run "ln -nfs #{shared_path}/config/flowplace_config.rb #{release_path}/config/flowplace_config.rb" 
+end
 
+task :fish do
+  run 'ls'
+end
+task :flowplace_install do
+#  top.upload_certs
+#  top.deprec.rails.install_stack
+  
+  sudo 'gem install rails --version 2.2.2 --no-ri --no-rdoc'
+#  sudo 'gem sources -a http://gems.github.com'
+#  sudo 'gem install mislav-will_paginate --no-ri --no-rdoc'
+#  top.deprec.db.install
+
+#  top.deploy.setup
+#  top.deploy.default
+
+#  top.deploy.migrate
+end
 namespace :deploy do
   task :restart, :roles => :app, :except => { :no_release => true } do
     top.deprec.app.restart
+  end
+  task :symlink_flowplace_config do
+    run "ln -nfs #{shared_path}/config/flowplace_config.rb #{current_path}/config/flowplace_config.rb" 
   end
 end
