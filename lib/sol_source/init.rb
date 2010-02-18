3c707afa7a50bbf6be2439d7f95de2b3739b1486
Lewis Hoffman <lewis@grasscommons.org>
2010-02-18 10:51:53 -0700

    fix mc_lib in init (shoulda been w/ last commit)

--- lib/sol_source/init.rb
+++ lib/sol_source/init.rb
@@ -2,10 +2,11 @@ sol_source_path = File.expand_path(File.dirname(__FILE__))
 SOL_DIR = "#{sol_source_path}/sols"
 
 $LOAD_PATH << sol_source_path
-
-require 'mc_lib/help'
+require 'rubygems'
+require 'mc_lib/mc_lib'
 %w( mc_lib sols ).each do |autoload_dir|
   Dir[ "#{sol_source_path}/#{autoload_dir}/**/*.rb" ].each do |f|
     load f
   end
 end
+
