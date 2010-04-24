ddc15b8321d540141f933a31cf4047a713b2385e
Eric Harris-Braun <eric@harris-braun.com>
2010-04-24 09:17:33 -0400

    fixed bug in detecting databaase type that was breaking user account searching RM#198

--- lib/sql.rb
+++ lib/sql.rb
@@ -1,3 +1,3 @@
 UsingPostgres = ActiveRecord::Base.connection.class.to_s == 'ActiveRecord::ConnectionAdapters::PostgreSQLAdapter'
-UsingMySQL = ActiveRecord::Base.connection.class.to_s == 'ActiveRecord::ConnectionAdapters::MySQLAdapter'
+UsingMySQL = ActiveRecord::Base.connection.class.to_s == 'ActiveRecord::ConnectionAdapters::MysqlAdapter'
 SQL_FULL_NAME = !UsingMySQL ? "(users.first_name||' '||users.last_name)" : "concat(users.first_name,' ',users.last_name)"
