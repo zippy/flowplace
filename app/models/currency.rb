zippy/flowplace

commit 2af0bf78e518bdf62477c6695b1e1717c4feceec

fixed bug that appeared when joining a user to a circle which uses an auto-join currency
If a currency was used by more than one cirlce then auto-joining was causing a duplicate joining error to be thrown.  Now added a check so as not to re-join a user to a currency twice in that case

app/models/currency.rb
@@ -698,13 +698,15 @@ class CurrencyMembrane
       if player_class == 'member' && namer
         self_player = currency_accounts.find_by_player_class('self')
         autojoin_currencies.each do |c|
-          play = {
-            'from' => namer,
-            'to' => player,
-            'currency' => c,
-            'player_class' => 'member'
-          }
-          api_play('grant',user,play)
+          if !user.player_clasess_in(self).include?('member')
+            play = {
+              'from' => namer,
+              'to' => player,
+              'currency' => c,
+              'player_class' => 'member'
+            }
+            api_play('grant',user,play)
+          end
         end
       end
       player

commit 3d818500291f4d8b51b6da5583a7fada29fb1745

fixed bug introduced in 2af0bf78e518bdf62477c6695b1e1717c4feceec

app/models/currency.rb
@@ -698,7 +698,7 @@ class CurrencyMembrane
       if player_class == 'member' && namer
         self_player = currency_accounts.find_by_player_class('self')
         autojoin_currencies.each do |c|
-          if !user.player_clasess_in(self).include?('member')
+          if !user.player_clasess_in(c).include?('member')
             play = {
               'from' => namer,
               'to' => player,